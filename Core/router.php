a1e85187989077b7b7827750224c7889910ecfe4
MatijaJurcic <matijajurcic2207@gmail.com>
2026-08-27 16:19:31 +0200

    refactor: add namespace autoloader and import Core\Response helper

--- Core/router.php
+++ Core/router.php
@@ -1,11 +1,10 @@
 <?php
 
-$routes = require('routes.php');
 
 function matchRoute($uri, $routes)
 {
   if (array_key_exists($uri, $routes)) {
-    return $routes[$uri];
+    return base_path($routes[$uri]);
   }
 
   abort();
@@ -15,10 +14,12 @@ function abort($code = 404)
 {
   http_response_code($code);
 
-  require "views/{$code}.php";
+  require base_path("views/{$code}.php");
 
   die();
 }
+
+$routes = require base_path('routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
