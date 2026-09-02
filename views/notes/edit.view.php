a5d6aa261d1ed9cabd49eb35ac397c31af0f1633
MatijaJurcic <matijajurcic2207@gmail.com>
2026-09-02 17:32:47 +0200

    feat: Updating With PATCH Requests

---
+++ views/notes/edit.view.php
@@ -0,0 +1,45 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <form method="POST" action="/note">
+      <input type="hidden" name="_method" value="PATCH">
+      <input type="hidden" name="id" value="<?= $note ['id'] ?>">
+      <div class="col-span-full">
+        <label for="body" class="block text-sm/6 font-medium text-white">Note Description:</label>
+        
+        <div class="mt-2">
+          <textarea
+            id="body"
+            name="body"
+            rows="3"
+            placeholder="Write your note here..."
+            class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+          ><?= $note['body'] ?></textarea>
+            <?php if (isset($errors['body'])) : ?>
+              <p class="mt-2 text-sm text-red-500"><?= $errors['body'] ?></p>
+            <?php endif; ?>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <a 
+            href="/notes" 
+            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Cancel
+          </a>
+          <button
+            type="submit"
+            class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Update
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</main>
+
+<?php require base_path('views/partials/footer.php') ?>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -8,14 +8,23 @@
             <a href="/notes" class="text-blue-500 underline">go back...</a>
         </p>
 
-        <p class="text-white"><?= htmlspecialchars($note['body']) ?></p>
+            <p class="text-white"><?= htmlspecialchars($note['body']) ?></p>
 
-         <form class="mt-6" method="POST">
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="hidden" name="id" value="<?= $note['id'] ?>">
-            <button class="text-sm text-red-500">Delete</button>
-        </form>
+        <div class="mt-6 flex items-center gap-x-4">
+            <form method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="text-sm text-red-500 hover:underline cursor-pointer">Delete</button>
+            </form>
+
+            <a href="/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+        Edit Note
+            </a>
+        </div>
+
     </div>
 </main>
 
 <?php require base_path('views/partials/footer.php') ?>
+
+  
